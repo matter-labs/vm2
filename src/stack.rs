9a3cab267612ce8c6e55f43629e8e67493f50c8c
Joonatan Saarhelo <joon.saar@gmail.com>
2024-04-28 20:35:43 +0200

    parallel cleaning of stacks

--- src/stack.rs
+++ src/stack.rs
@@ -1,4 +1,5 @@
 use crate::bitset::Bitset;
+use std::sync::mpsc;
 use u256::U256;
 
 #[derive(Clone, PartialEq, Debug)]
@@ -24,23 +25,64 @@ impl Stack {
     }
 }
 
-#[derive(Default)]
+const CLEANERS: usize = 4;
+
 pub struct StackPool {
-    stacks: Vec<Box<Stack>>,
+    clean_stack_source: mpsc::Receiver<Box<Stack>>,
+    dirty_stack_sinks: [mpsc::Sender<Box<Stack>>; CLEANERS],
+    current_cleaner: usize,
+}
+
+impl Default for StackPool {
+    fn default() -> Self {
+        let (clean_stack_sink, clean_stack_source) = mpsc::channel();
+
+        Self {
+            clean_stack_source,
+            current_cleaner: 0,
+
+            dirty_stack_sinks: (0..CLEANERS)
+                .map(|_| {
+                    let (dirty_stack_sink, dirty_stack_source) = mpsc::channel();
+                    let clean_stack_sink = clean_stack_sink.clone();
+
+                    std::thread::spawn(move || {
+                        for _ in 0..10 {
+                            if clean_stack_sink.send(Stack::new()).is_err() {
+                                return;
+                            }
+                        }
+
+                        loop {
+                            let Ok(mut stack): Result<Box<Stack>, _> = dirty_stack_source.recv()
+                            else {
+                                return;
+                            };
+                            stack.zero();
+                            if clean_stack_sink.send(stack).is_err() {
+                                return;
+                            }
+                        }
+                    });
+
+                    dirty_stack_sink
+                })
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap(),
+        }
+    }
 }
 
 impl StackPool {
     pub fn get(&mut self) -> Box<Stack> {
-        self.stacks
-            .pop()
-            .map(|mut s| {
-                s.zero();
-                s
-            })
-            .unwrap_or_else(Stack::new)
+        self.clean_stack_source.recv().unwrap()
     }
 
     pub fn recycle(&mut self, stack: Box<Stack>) {
-        self.stacks.push(stack);
+        self.dirty_stack_sinks[self.current_cleaner]
+            .send(stack)
+            .unwrap();
+        self.current_cleaner = (self.current_cleaner + 1) % CLEANERS;
     }
 }
