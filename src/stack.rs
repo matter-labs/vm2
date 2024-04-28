ff50eccfa1c8049c2ed7cc565ac1234e6227f68c
Joonatan Saarhelo <joon.saar@gmail.com>
2024-04-28 20:03:13 +0200

    nicer code for StackPool that produces better assembly, too

--- src/stack.rs
+++ src/stack.rs
@@ -7,6 +7,23 @@ pub struct Stack {
     pub slots: [U256; 1 << 16],
 }
 
+impl Stack {
+    fn new() -> Box<Self> {
+        Box::new(Stack {
+            pointer_flags: Default::default(),
+            slots: [U256::zero(); 1 << 16],
+        })
+    }
+    fn zero(&mut self) {
+        self.pointer_flags = Default::default();
+
+        // This loop results in just one call to _bzero unlike setting self.slots
+        for slot in self.slots.iter_mut() {
+            *slot = U256::zero();
+        }
+    }
+}
+
 #[derive(Default)]
 pub struct StackPool {
     stacks: Vec<Box<Stack>>,
@@ -17,16 +34,10 @@ impl StackPool {
         self.stacks
             .pop()
             .map(|mut s| {
-                s.slots = [U256::zero(); 1 << 16];
-                s.pointer_flags = Default::default();
+                s.zero();
                 s
             })
-            .unwrap_or_else(|| {
-                Box::new(Stack {
-                    pointer_flags: Default::default(),
-                    slots: [U256::zero(); 1 << 16],
-                })
-            })
+            .unwrap_or_else(Stack::new)
     }
 
     pub fn recycle(&mut self, stack: Box<Stack>) {
