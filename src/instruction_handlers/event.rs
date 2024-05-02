9cc6eb4efc5f071b9860f87a6e01626afdae0813
Joonatan Saarhelo <joon.saar@gmail.com>
2024-05-02 10:09:20 +0100

    feat: implement L2 to L1 message opcode (#8)
    * implement L2 to L1 message opcode
    
    * export L2ToL1Log

--- src/instruction_handlers/event.rs
+++ src/instruction_handlers/event.rs
@@ -2,7 +2,7 @@ use super::{common::instruction_boilerplate_with_panic, free_panic};
 use crate::{
     addressing_modes::{Arguments, Immediate1, Register1, Register2, Source},
     instruction::InstructionResult,
-    modified_world::Event,
+    modified_world::{Event, L2ToL1Log},
     Instruction, Predicate, VirtualMachine,
 };
 use u256::H160;
@@ -31,6 +31,28 @@ fn event(vm: &mut VirtualMachine, instruction: *const Instruction) -> Instructio
     })
 }
 
+fn l2_to_l1(vm: &mut VirtualMachine, instruction: *const Instruction) -> InstructionResult {
+    instruction_boilerplate_with_panic(vm, instruction, |vm, args, continue_normally| {
+        if vm.state.current_frame.is_static {
+            return free_panic(vm);
+        }
+
+        let key = Register1::get(args, &mut vm.state);
+        let value = Register2::get(args, &mut vm.state);
+        let is_service = Immediate1::get(args, &mut vm.state).low_u32() == 1;
+        vm.world.record_l2_to_l1_log(L2ToL1Log {
+            key,
+            value,
+            is_service,
+            address: vm.state.current_frame.address,
+            shard_id: 0,
+            tx_number: vm.state.transaction_number,
+        });
+
+        continue_normally
+    })
+}
+
 impl Instruction {
     pub fn from_event(
         key: Register1,
@@ -44,6 +66,21 @@ impl Instruction {
                 .write_source(&key)
                 .write_source(&value)
                 .write_source(&Immediate1(is_first.into())),
+        }
+    }
+
+    pub fn from_l2_to_l1_message(
+        key: Register1,
+        value: Register2,
+        is_service: bool,
+        predicate: Predicate,
+    ) -> Self {
+        Self {
+            handler: l2_to_l1,
+            arguments: Arguments::new(predicate, 34)
+                .write_source(&key)
+                .write_source(&value)
+                .write_source(&Immediate1(is_service.into())),
         }
     }
 }
