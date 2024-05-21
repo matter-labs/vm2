7b2a9c4d4d3f7089d5187fb949920a42c2a54dc2
Fedor Sakharov <fedor.sakharov@gmail.com>
2024-05-21 16:37:22 +0200

    correct tx number

--- src/instruction_handlers/precompiles.rs
+++ src/instruction_handlers/precompiles.rs
@@ -50,7 +50,7 @@ fn precompile_call(
         let query = LogQuery {
             timestamp: Timestamp(0),
             key: abi.to_u256(),
-            tx_number_in_block: 20,
+            tx_number_in_block: vm.state.transaction_number,
             aux_byte: Default::default(),
             shard_id: Default::default(),
             address: Default::default(),
