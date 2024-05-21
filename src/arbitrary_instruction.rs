7842f787db518d35431ff12334ccc75acaa84b28
mikhailUshakoff <mikhail.ushakov@yahoo.com>
2024-05-21 13:01:57 +0200

    Fix fuzz test

--- src/arbitrary_instruction.rs
+++ src/arbitrary_instruction.rs
@@ -1,3 +1,4 @@
+use crate::addressing_modes::Arguments;
 use crate::instruction_handlers::{
     Add, And, CallingMode, Div, Heap, Mul, Or, PtrAdd, PtrPack, PtrShrink, PtrSub, RotateLeft,
     RotateRight, ShiftLeft, ShiftRight, Sub, Xor,
@@ -13,13 +14,17 @@ impl<'a> Arbitrary<'a> for Instruction {
             u.arbitrary()?
         };
 
+        // 1 to 4 are reserved gas costs and also skip 0
+        let gas_cost = u.arbitrary::<u8>()?.checked_add(5).unwrap_or(255);
+        let arguments = Arguments::new(predicate, gas_cost as u32);
+
         Ok(match u.choose_index(23)? {
             0 => Self::from_binop::<Add>(
                 u.arbitrary()?,
                 u.arbitrary()?,
                 u.arbitrary()?,
                 (),
-                predicate,
+                arguments,
                 u.arbitrary()?,
                 u.arbitrary()?,
             ),
@@ -28,7 +33,7 @@ impl<'a> Arbitrary<'a> for Instruction {
                 u.arbitrary()?,
                 u.arbitrary()?,
                 (),
-                predicate,
+                arguments,
                 u.arbitrary()?,
                 u.arbitrary()?,
             ),
@@ -37,7 +42,7 @@ impl<'a> Arbitrary<'a> for Instruction {
                 u.arbitrary()?,
                 u.arbitrary()?,
                 (),
-                predicate,
+                arguments,
                 u.arbitrary()?,
                 u.arbitrary()?,
             ),
@@ -46,7 +51,7 @@ impl<'a> Arbitrary<'a> for Instruction {
                 u.arbitrary()?,
                 u.arbitrary()?,
                 (),
-                predicate,
+                arguments,
                 u.arbitrary()?,
                 u.arbitrary()?,
             ),
@@ -55,7 +60,7 @@ impl<'a> Arbitrary<'a> for Instruction {
                 u.arbitrary()?,
                 u.arbitrary()?,
                 (),
-                predicate,
+                arguments,
                 u.arbitrary()?,
                 u.arbitrary()?,
             ),
@@ -64,7 +69,7 @@ impl<'a> Arbitrary<'a> for Instruction {
                 u.arbitrary()?,
                 u.arbitrary()?,
                 (),
-                predicate,
+                arguments,
                 u.arbitrary()?,
                 u.arbitrary()?,
             ),
@@ -73,7 +78,7 @@ impl<'a> Arbitrary<'a> for Instruction {
                 u.arbitrary()?,
                 u.arbitrary()?,
                 (),
-                predicate,
+                arguments,
                 u.arbitrary()?,
                 u.arbitrary()?,
             ),
@@ -82,7 +87,7 @@ impl<'a> Arbitrary<'a> for Instruction {
                 u.arbitrary()?,
                 u.arbitrary()?,
                 (),
-                predicate,
+                arguments,
                 u.arbitrary()?,
                 u.arbitrary()?,
             ),
@@ -91,7 +96,7 @@ impl<'a> Arbitrary<'a> for Instruction {
                 u.arbitrary()?,
                 u.arbitrary()?,
                 (),
-                predicate,
+                arguments,
                 u.arbitrary()?,
                 u.arbitrary()?,
             ),
@@ -100,7 +105,7 @@ impl<'a> Arbitrary<'a> for Instruction {
                 u.arbitrary()?,
                 u.arbitrary()?,
                 (),
-                predicate,
+                arguments,
                 u.arbitrary()?,
                 u.arbitrary()?,
             ),
@@ -109,7 +114,7 @@ impl<'a> Arbitrary<'a> for Instruction {
                 u.arbitrary()?,
                 u.arbitrary()?,
                 u.arbitrary()?,
-                predicate,
+                arguments,
                 u.arbitrary()?,
                 u.arbitrary()?,
             ),
@@ -118,60 +123,60 @@ impl<'a> Arbitrary<'a> for Instruction {
                 u.arbitrary()?,
                 u.arbitrary()?,
                 u.arbitrary()?,
-                predicate,
+                arguments,
                 u.arbitrary()?,
                 u.arbitrary()?,
             ),
-            12 => Self::from_jump(u.arbitrary()?, predicate),
+            12 => Self::from_jump(u.arbitrary()?, arguments),
             13 => Self::from_ptr::<PtrAdd>(
                 u.arbitrary()?,
                 u.arbitrary()?,
                 u.arbitrary()?,
-                predicate,
+                arguments,
                 u.arbitrary()?,
             ),
             14 => Self::from_ptr::<PtrSub>(
                 u.arbitrary()?,
                 u.arbitrary()?,
                 u.arbitrary()?,
-                predicate,
+                arguments,
                 u.arbitrary()?,
             ),
             15 => Self::from_ptr::<PtrShrink>(
                 u.arbitrary()?,
                 u.arbitrary()?,
                 u.arbitrary()?,
-                predicate,
+                arguments,
                 u.arbitrary()?,
             ),
             16 => Self::from_ptr::<PtrPack>(
                 u.arbitrary()?,
                 u.arbitrary()?,
                 u.arbitrary()?,
-                predicate,
+                arguments,
                 u.arbitrary()?,
             ),
             17 => {
-                Self::from_load::<Heap>(u.arbitrary()?, u.arbitrary()?, u.arbitrary()?, predicate)
+                Self::from_load::<Heap>(u.arbitrary()?, u.arbitrary()?, u.arbitrary()?, arguments)
             }
             18 => Self::from_store::<Heap>(
                 u.arbitrary()?,
                 u.arbitrary()?,
                 u.arbitrary()?,
-                predicate,
+                arguments,
                 false,
             ),
             19 => {
-                Self::from_load_pointer(u.arbitrary()?, u.arbitrary()?, u.arbitrary()?, predicate)
+                Self::from_load_pointer(u.arbitrary()?, u.arbitrary()?, u.arbitrary()?, arguments)
             }
-            20 => Self::from_sstore(u.arbitrary()?, u.arbitrary()?, predicate),
-            21 => Self::from_sload(u.arbitrary()?, u.arbitrary()?, predicate),
+            20 => Self::from_sstore(u.arbitrary()?, u.arbitrary()?, arguments),
+            21 => Self::from_sload(u.arbitrary()?, u.arbitrary()?, arguments),
             22 => Self::from_far_call::<{ CallingMode::Normal as u8 }>(
                 u.arbitrary()?,
                 u.arbitrary()?,
                 u.arbitrary()?,
                 u.arbitrary()?,
-                predicate,
+                arguments,
             ),
             _ => unreachable!(),
         })
