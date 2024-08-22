c339dde79db8bec214faf5b7e15d2a2a70c4a684
Joonatan Saarhelo <joon.saar@gmail.com>
2024-08-22 19:12:42 +0200

    inline bitset

--- src/bitset.rs
+++ src/bitset.rs
@@ -2,22 +2,26 @@
 pub struct Bitset([u64; 1 << 10]);
 
 impl Bitset {
+    #[inline(always)]
     pub fn get(&self, i: u16) -> bool {
         let (slot, bit) = slot_and_bit(i);
         self.0[slot] & bit != 0
     }
 
+    #[inline(always)]
     pub fn set(&mut self, i: u16) {
         let (slot, bit) = slot_and_bit(i);
         self.0[slot] |= bit;
     }
 
+    #[inline(always)]
     pub fn clear(&mut self, i: u16) {
         let (slot, bit) = slot_and_bit(i);
         self.0[slot] &= !bit;
     }
 }
 
+#[inline(always)]
 fn slot_and_bit(i: u16) -> (usize, u64) {
     ((i >> 6) as usize, 1u64 << (i & 0b111111))
 }
