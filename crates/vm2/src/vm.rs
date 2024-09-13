d754ca437aca6a50db661e5b25694f768b8fff24
Alex Ostrovski <aov@matterlabs.dev>
2024-09-13 10:38:22 +0300

    Place snapshot inside VM

--- crates/vm2/src/vm.rs
+++ crates/vm2/src/vm.rs
@@ -12,7 +12,7 @@ use crate::{
     stack::StackPool,
     state::{State, StateSnapshot},
     world_diff::{ExternalSnapshot, Snapshot, WorldDiff},
-    ExecutionEnd, Instruction, ModeRequirements, Predicate, Program,
+    ExecutionEnd, Instruction, ModeRequirements, Predicate, Program, World,
 };
 
 /// [`VirtualMachine`] settings.
@@ -34,6 +34,7 @@ pub struct VirtualMachine<T, W> {
     /// Instruction that is jumped to when things go wrong while executing another.
     /// Boxed, so the pointer isn't invalidated by moves.
     pub(crate) panic: Box<Instruction<T, W>>,
+    pub(crate) snapshot: Option<VmSnapshot>,
 }
 
 impl<T: Tracer, W> VirtualMachine<T, W> {
@@ -66,22 +67,24 @@ impl<T: Tracer, W> VirtualMachine<T, W> {
                 None,
                 Arguments::new(Predicate::Always, RETURN_COST, ModeRequirements::none()),
             )),
+            snapshot: None,
         }
     }
 
-    /// Provides a reference to the [`World`](crate::World) diff accumulated by VM execution so far.
+    /// Provides a reference to the [`World`] diff accumulated by VM execution so far.
     pub fn world_diff(&self) -> &WorldDiff {
         &self.world_diff
     }
 
-    /// Provides a mutable reference to the [`World`](crate::World) diff accumulated by VM execution so far.
+    /// Provides a mutable reference to the [`World`] diff accumulated by VM execution so far.
     ///
-    /// It is unsound to mutate `WorldDiff` in the middle of VM execution in the general case; thus, this method should only be used in tests.
+    /// It is unsound to mutate [`WorldDiff`] in the middle of VM execution in the general case; thus, this method should only be used in tests.
     #[doc(hidden)]
     pub fn world_diff_mut(&mut self) -> &mut WorldDiff {
         &mut self.world_diff
     }
 
+    /// Runs this VM with the specified [`World`] and [`Tracer`] until an end of execution due to a hook, or an error.
     pub fn run(&mut self, world: &mut W, tracer: &mut T) -> ExecutionEnd {
         unsafe {
             loop {
@@ -128,44 +131,66 @@ impl<T: Tracer, W> VirtualMachine<T, W> {
             .map(|left| (left, end))
     }
 
-    /// Returns a compact representation of the VM's current state,
-    /// including pending side effects like storage changes and emitted events.
-    /// [`Self::rollback()`] can be used to return the VM to this state.
+    /// Creates a VM snapshot. The snapshot can then be rolled back to, or discarded.
     ///
     /// # Panics
     ///
-    /// Calling this function outside the initial callframe is not allowed.
-    pub fn snapshot(&self) -> VmSnapshot {
+    /// - Panics if called outside the initial (bootloader) callframe.
+    /// - Panics if this VM already has a snapshot.
+    pub fn make_snapshot(&mut self) {
+        assert!(self.snapshot.is_none(), "VM already has a snapshot");
         assert!(
             self.state.previous_frames.is_empty(),
-            "Snapshotting is only allowed in the bootloader!"
+            "Snapshotting is only allowed in the bootloader"
         );
-        VmSnapshot {
+
+        self.snapshot = Some(VmSnapshot {
             world_snapshot: self.world_diff.external_snapshot(),
             state_snapshot: self.state.snapshot(),
-        }
+        });
     }
 
-    /// Returns the VM to the state it was in when the snapshot was created.
+    /// Returns the VM to the state it was in when [`Self::make_snapshot()`] was called. If there is
     ///
     /// # Panics
     ///
-    /// - Rolling back snapshots in anything but LIFO order may panic.
-    /// - Rolling back outside the initial callframe will panic.
-    pub fn rollback(&mut self, snapshot: VmSnapshot) {
+    /// - Panics if this VM doesn't hold a snapshot.
+    /// - Panics if called outside the initial (bootloader) callframe.
+    pub fn rollback(&mut self) {
         assert!(
             self.state.previous_frames.is_empty(),
-            "Rolling back is only allowed in the bootloader!"
+            "Rolling back is only allowed in the bootloader"
         );
+
+        let snapshot = self
+            .snapshot
+            .take()
+            .expect("`rollback()` called without a snapshot");
         self.world_diff.external_rollback(snapshot.world_snapshot);
         self.state.rollback(snapshot.state_snapshot);
+        self.delete_history();
+    }
+
+    /// Pops a [previously made](Self::make_snapshot()) snapshot without rolling back to it. This effectively commits
+    /// all changes made up to this point, so that they cannot be rolled back.
+    ///
+    /// # Panics
+    ///
+    /// - Panics if called outside the initial (bootloader) callframe.
+    pub fn pop_snapshot(&mut self) {
+        assert!(
+            self.state.previous_frames.is_empty(),
+            "Popping a snapshot is only allowed in the bootloader"
+        );
+        self.snapshot = None;
+        self.delete_history();
     }
 
     /// This must only be called when it is known that the VM cannot be rolled back,
     /// so there must not be any external snapshots and the callstack
     /// should ideally be empty, though in practice it sometimes contains
     /// a near call inside the bootloader.
-    pub fn delete_history(&mut self) {
+    fn delete_history(&mut self) {
         self.world_diff.delete_history();
         self.state.delete_history();
     }
