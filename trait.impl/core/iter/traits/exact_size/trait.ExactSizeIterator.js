1eae25105ff7b18fa3aa717be0fbc7fb0c099974
joonazan <joon.saar@gmail.com>
2025-03-05 14:36:28 +0000

    Deploying to gh-pages from @ matter-labs/vm2@84ffde96aa4ee5d605c719eba9a604a83cfc1b7e 🚀

---
+++ trait.impl/core/iter/traits/exact_size/trait.ExactSizeIterator.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"zksync_vm2":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/exact_size/trait.ExactSizeIterator.html\" title=\"trait core::iter::traits::exact_size::ExactSizeIterator\">ExactSizeIterator</a> for <a class=\"struct\" href=\"zksync_vm2/precompiles/struct.PrecompileMemoryReader.html\" title=\"struct zksync_vm2::precompiles::PrecompileMemoryReader\">PrecompileMemoryReader</a>&lt;'_&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
