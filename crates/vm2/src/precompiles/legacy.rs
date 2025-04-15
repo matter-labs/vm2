7f39c7da18860c433ba8d099af874e17d48f86f0
Raid5594 <52794079+Raid5594@users.noreply.github.com>
2025-04-15 23:15:58 +0400

    feat: support for precompiles returning 3 words (#89)
    # What ❔
    
    <!-- What are the changes this PR brings about? -->
    <!-- Example: This PR adds a PR template to the repo. -->
    <!-- (For bigger PRs adding more context is appreciated) -->
    
    ## Why ❔
    
    <!-- Why are these changes done? What goal do they contribute to? What
    are the principles behind them? -->
    <!-- Example: PR templates ensure PR reviewers, observers, and future
    iterators are in context about the evolution of repos. -->
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [ ] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [ ] Tests for the changes have been added / updated.
    - [ ] Documentation comments have been added / updated.
    - [ ] Code has been formatted via `cargo fmt` and `cargo clippy`.

--- crates/vm2/src/precompiles/legacy.rs
+++ crates/vm2/src/precompiles/legacy.rs
@@ -73,9 +73,16 @@ impl Memory for LegacyIo<'_> {
     ) -> MemoryQuery {
         let start_word = query.location.index.0;
         if query.rw_flag {
-            assert!(start_word < 2, "standard precompiles never write >2 words");
-            self.output.buffer[start_word as usize] = query.value;
-            self.output.len = self.output.len.max(start_word + 1);
+            assert!(start_word < 3, "standard precompiles never write >3 words");
+            // start_word == 2 only for EcAdd and EcMul precompiles
+            if start_word == 2 {
+                self.output.buffer[0] = self.output.buffer[1];
+                self.output.buffer[1] = query.value;
+                self.output.len = self.output.len.max(start_word);
+            } else {
+                self.output.buffer[start_word as usize] = query.value;
+                self.output.len = self.output.len.max(start_word + 1);
+            }
         } else {
             // Access `Heap` directly for a speed-up
             query.value = self.input.heap.read_u256(start_word * 32);
